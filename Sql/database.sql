3db5078aa8cab62034646afc4a3a3230b14a3d39
gabrielhrf <79175866+gabrielhrf@users.noreply.github.com>
2021-11-10 09:35:21 -0300

    Update database.sql

--- Sql/database.sql
+++ Sql/database.sql
@@ -14,3 +14,7 @@ create table compra(
     id_boleto int not null,
     telefone_comprador varchar(11) not null
 );
+
+insert into produto (nome, valor) values ('Folha A4', 19.8);
+insert into compra (link_pdf, id_boleto, telefone_comprador) values ('https://download.gerencianet.com.br/93084_3656_CHOZE0/93084-1623-DONEM8.pdf?sandbox=true', 1438850, '3299887766');
+
